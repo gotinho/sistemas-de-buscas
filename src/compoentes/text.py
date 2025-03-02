e6f03fd133d17f6700744fd466e9057ea565f048
gotinho <gotinho@users.noreply.github.com>
2025-03-02 17:05:19 -0300

    definir cor do texto

--- src/compoentes/text.py
+++ src/compoentes/text.py
@@ -2,11 +2,11 @@ import pygame
 
 
 class Text:
-    def __init__(self, text, posicao, tamanho=15):
+    def __init__(self, text, posicao, tamanho=15, cor=(255, 255, 255)):
         self.text = text
         self.posicao = posicao
         self.font = pygame.font.SysFont(None, tamanho)
-        self.color = (255, 255, 255)
+        self.cor = cor
         self.draw()
 
     def set_text(self, text):
@@ -14,7 +14,7 @@ class Text:
         self.draw()
 
     def draw(self):
-        self.display_text = self.font.render(self.text, True, self.color)
+        self.display_text = self.font.render(self.text, True, self.cor)
 
     def render(self, display):
         display.blit(self.display_text, self.posicao)
