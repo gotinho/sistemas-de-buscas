29a84755a67e1b261614ed0189b09cb691ba374c
Igor Alexandre Goto <gotinho@users.noreply.github.com>
2025-03-25 21:48:52 -0300

    Update tsp_formiga.py

--- src/tsp_formiga.py
+++ src/tsp_formiga.py
@@ -1,6 +1,8 @@
 import gzip
 import random
+
 # import io
+import numpy as np
 
 
 def calcula_distancia(p1, p2):
@@ -30,7 +32,8 @@ def calcula_heuristica(distancia):
 
 
 def calcula_transicao(feromonio, heuristica, peso_feromonio, peso_heuristica):
-    pow(feromonio, peso_feromonio) * pow(heuristica, peso_heuristica)
+    return pow(feromonio, peso_feromonio) * pow(heuristica, peso_heuristica)
+
 
 class Formiga:
     def __init__(self, inicial):
@@ -38,42 +41,81 @@ class Formiga:
         self.custo = 0
 
     def escolher_cidades(
-        self, matriz_heuristica, matriz_feromonio, peso_heuristica, peso_feromonio
+        self,
+        matriz_heuristica,
+        matriz_feromonio,
+        peso_heuristica,
+        peso_feromonio,
+        atlas,
     ):
         posicao = self.visitadas[0]
         disponiveis = [i for i in range(len(matriz_heuristica))]
         disponiveis.remove(posicao)
-
-        transicao = [
-            calcula_transicao(
-                matriz_feromonio[posicao, j],
-                matriz_heuristica[posicao, j],
-                peso_feromonio,
-                peso_heuristica,
-            )
-            for j in disponiveis
-        ]
-        soma = sum(transicao)
-        maior = 0
-        maior_probabilidade = 0
-        for j in disponiveis:
-            probabilidade = (
+        while len(disponiveis) > 0:
+            transicao = [
                 calcula_transicao(
-                    matriz_feromonio[posicao, j],
-                    matriz_heuristica[posicao, j],
+                    matriz_feromonio[posicao][j],
+                    matriz_heuristica[posicao][j],
                     peso_feromonio,
                     peso_heuristica,
                 )
-                / soma
-            )
+                for j in disponiveis
+            ]
+            soma = sum(transicao)
+            if soma == 0:
+                print("soma 0 ",disponiveis)
+            maior = -1
+            maior_probabilidade = -1
+            for j in disponiveis:
+                probabilidade = (
+                    calcula_transicao(
+                        matriz_feromonio[posicao][j],
+                        matriz_heuristica[posicao][j],
+                        peso_feromonio,
+                        peso_heuristica,
+                    )
+                    / soma
+                )
+                if(probabilidade < 0):
+                    print ("menor",probabilidade)
+                if probabilidade > maior_probabilidade:
+                    maior = j
+                    maior_probabilidade = probabilidade
+            posicao = maior
+            if(posicao not in disponiveis):
+                print("nnn",posicao)
+                print("nnn",disponiveis)
+            disponiveis.remove(posicao)
+            self.visitadas.append(posicao)
+        self.calcular_custo(atlas)
 
-            if probabilidade > maior_probabilidade:
-                maior = j
-                maior_probabilidade = probabilidade
-        posicao = maior
-        self.visitadas.append(posicao)
-    
-   
+    def calcular_custo(self, atlas):
+        for i in range(len(self.visitadas)):
+            p1 = self.visitadas[i] + 1
+            if i < len(self.visitadas) - 1:
+                p2 = self.visitadas[i + 1] + 1
+                self.custo += atlas[p1][p2]
+            else:
+                p2 = self.visitadas[0] + 1
+                self.custo += atlas[p1][p2]
+
+    def reforco_feromonio(self, nivel_feromonio, matriz_feromonio):
+        for i in range(len(self.visitadas)):
+            p1 = self.visitadas[i]
+            if i < len(self.visitadas) - 1:
+                p2 = self.visitadas[i + 1]
+                matriz_feromonio[p1][p2] = matriz_feromonio[p1][p2] + nivel_feromonio
+            else:
+                p2 = self.visitadas[0] + 1
+                matriz_feromonio[p1][p2] = matriz_feromonio[p1][p2] + nivel_feromonio
+        return matriz_feromonio
+
+    def __lt__(self, outro):
+        return self.custo < outro.custo
+
+
+def criar_formigas(dimensao, n_formigas):
+    return [Formiga(n) for n in random.sample(range(dimensao), n_formigas)]
 
 
 def ler_arquivo_tsp_gz(caminho_arquivo):
@@ -127,31 +169,67 @@ def ler_arquivo_tsp_gz(caminho_arquivo):
 
 def main():
     caminho_do_arquivo = "data/ali535.tsp.gz"  # Substitua pelo caminho do seu arquivo
+    # caminho_do_arquivo = "data/a280.tsp.gz"  # Substitua pelo caminho do seu arquivo
     dados = ler_arquivo_tsp_gz(caminho_do_arquivo)
 
     if dados:
-        print("Nome do problema:", dados["nome"])
+        # print("Nome do problema:", dados["nome"])
         print("Dimensão:", dados["dimensao"])
         # print("Coordenadas:", dados["coordenadas"])
         dimensao = dados["dimensao"]
         atlas = calcula_atlas(dados["coordenadas"])
 
-        n_formigas = 5
+        c = 0
+        aa = random.sample(range(dimensao), dimensao)
+        print(len(aa))
+        for i in range(len(aa)):
+            p1 = aa[i] + 1
+            if i < len(aa) - 1:
+                p2 = aa[i + 1] + 1
+                c += atlas[p1][p2]
+            else:
+                p2 = aa[0] + 1
+                c += atlas[p1][p2]
+        print(c)
+
+        n_formigas = 50
         n_interacoes = 5
         peso_feromonio = 1
         peso_heuristica = 2
         taxa_evaporacao = 0.5
         feromonio_inicial = 1
-        matriz_feromonio = [
-            [feromonio_inicial for _ in range(dimensao)] for _ in range(dimensao)
-        ]
-        matriz_heuristica = [
-            [calcula_heuristica(atlas[i + 1][j + 1]) for j in range(dimensao)]
-            for i in range(dimensao)
-        ]
+        matriz_feromonio = np.array(
+            [[feromonio_inicial for _ in range(dimensao)] for _ in range(dimensao)]
+        )
+        matriz_heuristica = np.array(
+            [
+                [calcula_heuristica(atlas[i + 1][j + 1]) for j in range(dimensao)]
+                for i in range(dimensao)
+            ]
+        )
 
-        f = Formiga(10)
-        f.escolher_cidades(matriz_heuristica, matriz_feromonio)
+        for _ in range(n_interacoes):
+            formigas = criar_formigas(dimensao, n_formigas)
+            for f in formigas:
+                f.escolher_cidades(
+                    matriz_heuristica,
+                    matriz_feromonio,
+                    peso_heuristica,
+                    peso_feromonio,
+                    atlas,
+                )
+            formigas.sort()
+            melhor_rota = formigas[0]
+            custo_total = melhor_rota.custo
+            print(custo_total)
+            print(len(melhor_rota.visitadas))
+            nivel_feromonio = 1 / custo_total
+            matriz_feromonio = matriz_feromonio * taxa_evaporacao
+            matriz_feromonio = melhor_rota.reforco_feromonio(
+                nivel_feromonio, matriz_feromonio
+            )
+
+        print("Fim")
 
 
 if __name__ == "__main__":
